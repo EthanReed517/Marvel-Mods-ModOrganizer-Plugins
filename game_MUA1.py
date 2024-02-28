c35f2e243f18fe02e6362cd7d693ca351eff1b68
ak2yny <92672223+ak2yny@users.noreply.github.com>
2024-02-28 20:21:16 +0100

    Update Plugins with Structure Info
    Adding structure information (game folders) enables automatic detection of the main folder and simplifies installation. It also shows if a mod has folders that the game doesn't support. It unfortunately also marks mods as such, if they don't have any folders and only modify files in the main directory, but I don't think that's a problem.

--- game_MUA1.py
+++ game_MUA1.py
@@ -7,12 +7,48 @@ except:
 
 import mobase
 from ..basic_game import BasicGame
-from ..basic_features import BasicGameSaveGameInfo
+
+class MUA1ModDataChecker(mobase.ModDataChecker):
+    def __init__(self):
+        super().__init__()
+        self.validDirNames = [
+            "actors",
+            "automaps",
+            "conversations",
+            "data",
+            "dialogs",
+            "effects",
+            "hud",
+            "maps",
+            "models",
+            "motionpaths",
+            "movies",
+            "packages",
+            "plugins",
+            "scripts",
+            "shaders",
+            "skybox",
+            "sounds",
+            "subtitles",
+            "texs",
+            "textures",
+            "ui"
+        ]
+
+    def dataLooksValid(
+        self, tree: mobase.IFileTree
+    ) -> mobase.ModDataChecker.CheckReturn:
+        for entry in tree:
+            if not entry.isDir():
+                continue
+            if entry.name().casefold() in self.validDirNames:
+                return mobase.ModDataChecker.VALID
+        return mobase.ModDataChecker.INVALID
 
 class MarvelUltimateAllianceGame(BasicGame):
     Name = "Marvel - Ultimate Alliance Support Plugin"
     Author = "MrKablamm0fish, ak2yny, Rampage, and BaconWizard17"
-    Version = "2.0.0"
+    Version = "2.0.1"
 
     GameName = "Marvel - Ultimate Alliance"
     GameShortName = "mua1"
@@ -20,10 +56,16 @@ class MarvelUltimateAllianceGame(BasicGame):
     GameNexusId = 000
     GameSteamId = 000000
     GameBinary = "game.exe"
+    GameLauncher = "MUA.exe"
     GameDataPath = ""
     GameSaveExtension = "save"
     GameDocumentsDirectory = "%DOCUMENTS%/Activision/Marvel Ultimate Alliance"
     GameSavesDirectory = "%GAME_DOCUMENTS%/Save"
+
+    def init(self, organizer: mobase.IOrganizer) -> bool:
+        super().init(organizer)
+        self._featureMap[mobase.ModDataChecker] = MUA1ModDataChecker()
+        return True
 
     def executables(self):
         return [
